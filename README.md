b80cbf17b36cb13291890fd07cfcf5cbf86c2ff1
NotVentulus <o.svedgard@gmail.com>
2016-09-09 10:24:15 +0200

    lagt till ints för symboler och gamestates

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # 3-i-rad
 Projektbeskrivning
 
+
 Logg
+<p>10:20  Skapa ints för symboler och gamestates</p>
 
 Todo
+<p></p>
--- src/class1.java
+++ src/class1.java
@@ -1,6 +1,17 @@
-/**
- * Created by svos14 on 2016-09-09.
- */
+import java.util.Scanner;
+
 public class class1 {
-    testcommit
+    public static final int TOM = 0;
+    public static final int KRYSS = 1;
+    public static final int CIRKEL = 2;
+
+    public static final int SPELAR = 0;
+    public static final int OAVGJORT = 1;
+    public static final int KRYSS_VANN = 2;
+    public static final int CIRKEL_VANN = 3;
+
+    public static final int RADER = 3, KOLUMNER = 3;
+    public static int[][] bräde = new int[RADER][KOLUMNER];
+
+
 }
