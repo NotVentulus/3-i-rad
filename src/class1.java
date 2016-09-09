06ffdb0d187a00ae63d08bf77926fb1df07e7d72
NotVentulus <o.svedgard@gmail.com>
2016-09-09 11:11:20 +0200

    Lägga in meddelanden för gamestates i slutet

--- src/class1.java
+++ src/class1.java
@@ -13,5 +13,30 @@ public class class1 {
     public static final int RADER = 3, KOLUMNER = 3;
     public static int[][] bräde = new int[RADER][KOLUMNER];
 
+    public static int nuvarandeLäge;
+    public static int nuvarandeSpelare;
+    public static int nuvarandeRad, nuvarandeKolumn;
 
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+        startaSpel();
+        do{
+            spelarDrag(nuvarandeSpelare);
+            updatera(nuvarandeSpelare, nuvarandeRad, nuvarandeKolumn);
+            printBräde();
+
+            if (nuvarandeLäge == KRYSS_VANN){
+                System.out.println("Kryss vann!");
+            }
+            else if (nuvarandeLäge == CIRKEL_VANN){
+                System.out.println("Cirkel vann!");
+            }
+            else if (nuvarandeLäge == OAVGJORT){
+                System.out.println("Oavgjort!");
+            }
+            nuvarandeSpelare = (nuvarandeSpelare == KRYSS) ? CIRKEL : KRYSS;
+
+        }
+    }
 }
